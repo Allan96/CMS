a3f56ad4716c26a3ccfaeaae0696afb5b6949c09
Allan96 <allan.souza.silva@usp.br>
2019-07-20 19:40:27 -0300

    Página principal 80%

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$('#noticias').flickity({
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    groupCells: true,
+    pageDots: false,
+    freeScroll: true,
+    wrapAround: true
+});
