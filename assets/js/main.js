14bf9d0463d5c83305d67663ccf80ecd8b48a89a
Allan96 <allan.souza.silva@usp.br>
2019-07-21 16:45:23 -0300

    Adicionando efeitos de transição + a div mais informações

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,38 @@ $('#noticias').flickity({
     freeScroll: true,
     wrapAround: true
 });
+
+$(document).ready(function() {
+    $('.info-mais-div .close-icon').click(function(e) {
+        e.preventDefault();
+        $('.info-mais').removeClass('open');
+        $(".info-mais-div").animate({
+            right: "-300px",
+        }, 1000, function() {
+            // Animation complete.
+        });
+
+    });
+
+    $('.info-mais').click(function(e) {
+        e.preventDefault();
+        var open = $('.info-mais').hasClass('open');
+        if (!open) {
+            $('.info-mais').addClass('open');
+            $(".info-mais-div").animate({
+                right: "0px",
+            }, 1000, function() {
+                // Animation complete.
+            });
+        } else {
+            $('.info-mais').removeClass('open');
+            $(".info-mais-div").animate({
+                right: "-300px",
+            }, 1000, function() {
+                // Animation complete.
+            });
+        }
+
+    });
+
+});
